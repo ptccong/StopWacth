fa0171836632274f9721ec9faddf1f80f87e3859
ptccong <congpt.bhsoft@gmail.com>
2020-06-25 15:59:27 +0700

    Initial commit

---
+++ StopWacth/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace StopWacth
+{
+    class Wacth
+    {
+        Stopwatch stw = new Stopwatch();
+        public void start()
+        {
+            stw.Start();
+        }
+        public void stop()
+        {
+            for(int i = 0; i <= 20; i++)
+            {
+                Thread.Sleep(10);
+            }
+            stw.Stop();
+        }
+        public void show()
+        {
+            Console.WriteLine("Time :{0}", stw.Elapsed);
+        }
+       
+    }
+    class Program
+    {
+        public static void Main()
+        {
+            Wacth w = new Wacth();
+            w.start();
+            w.stop();
+            w.show();
+        }
+    }
+}
